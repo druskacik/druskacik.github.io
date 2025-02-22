bd8d3b52215a988adf4461c97ca168d539a1efcc
Robert Druska <robert.druska@gmail.com>
2025-02-22 21:00:16 +0100

    umami

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,4 +4,11 @@ export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
   modules: ['@nuxt/fonts', '@nuxt/content'],
+  extends: ['nuxt-umami'],
+  appConfig: {
+    umami: {
+      id: 'f92f8d2b-4b83-4f8e-be0e-27c8e1844783',
+      host: 'https://umami.cr.bswatcher.com', 
+    }
+  },
 })
